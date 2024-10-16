8dd3e40afd8f6ea7a2fb1fb1cc78557b45ced8eb
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-10-16 15:10:09 +0700

    complete appbar frame

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,53 @@
-import { Box } from '@mui/material'
+import { Badge, Box, Button, TextField, Tooltip, Typography } from '@mui/material'
 import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloLogo } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import WorkSpaces from './Menus/WorkSpaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profile from './Menus/Profiles'
 
-function index() {
+function AppBar() {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.main',
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={TrelloLogo} inheritViewBox sx={{ color: 'primary.main' }}/>
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
+        </Box>
+        <WorkSpaces />
+        <Recent />
+        <Starred />
+        <Templates />
+
+        <Button variant="outlined">Create</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+        <ModeSelect />
+        <Tooltip title='Notifications'>
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title='Help' sx={{ cursor: 'pointer' }}>
+          <HelpOutlineIcon />
+        </Tooltip>
+        <Profile />
+      </Box>
     </Box>
   )
 }
 
-export default index
+export default AppBar
--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material'
 function BoardContent() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.light',
+      backgroundColor: 'success.light',
       width: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
       display: 'flex',
