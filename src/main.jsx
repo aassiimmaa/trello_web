5ed144bda65c962218ec081158ffad7cb5da61b8
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2025-01-21 09:49:49 +0700

    add flash message react toastify

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import theme from '~/theme'
 
+//Config react toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} disableNestedContext>
       <CssBaseline />
-      < App />
+      <App />
+      <ToastContainer position='bottom-left' autoClose={3000} theme='colored' />
     </ThemeProvider>
   </React.StrictMode>
 )
