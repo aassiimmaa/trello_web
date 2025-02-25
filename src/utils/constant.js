81a730cba4ceac938a6755d306956d8c09c56071
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2025-02-25 10:50:49 +0700

    fix build mode

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,10 @@
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+} else if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-yuuf.onrender.com'
+}
+
+export const API_ROOT = apiRoot
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-yuuf.onrender.com'
+// export const API_ROOT = 'https://trello-api-yuuf.onrender.com'
